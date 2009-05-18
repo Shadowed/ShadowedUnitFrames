290c18a0ddf27df8172e30f743a495166a319e78
Shadowed <shadowed.wow@gmail.com>
2009-05-18 15:41:10 -0700

    * Made white more white

--- modules/totems.lua
+++ modules/totems.lua
@@ -1,5 +1,5 @@
 local Totems = ShadowUF:NewModule("Totems")
-local totemColors = {{r = 1, g = 0, b = 0.4}, {r = 0, g = 1, b = 0.4}, {r = 0, g = 0.4, b = 1}, {r = 0.80, g = 0.80, b = 0.80}}
+local totemColors = {{r = 1, g = 0, b = 0.4}, {r = 0, g = 1, b = 0.4}, {r = 0, g = 0.4, b = 1}, {r = 0.90, g = 0.90, b = 0.90}}
 ShadowUF:RegisterModule(Totems, "totemBar", ShadowUFLocals["Totem indicators"], "bar")
 
 function Totems:UnitEnabled(frame, unit)
