2e31a1225b034a6076c9bfb626cf50169882e6de
Shadowed <shadowed.wow@gmail.com>
2009-12-07 15:15:52 -0800

    Optimized rune bar, no longer looping through each rune to find the one to update

--- modules/runes.lua
+++ modules/runes.lua
@@ -1,6 +1,6 @@
 local Runes = {}
 local RUNE_MAP = {[1] = 1, [2] = 2, [3] = 5, [4] = 6, [5] = 3, [6] = 4}
-local runeColors
+local runeColors = {{r = 1, g = 0, b = 0.4}, {r = 0, g = 1, b = 0.4}, {r = 0, g = 0.4, b = 1}, {r = 0.7, g = 0.5, b = 1}}
 ShadowUF:RegisterModule(Runes, "runeBar", ShadowUFLocals["Rune bar"], true, "DEATHKNIGHT")
 
 function Runes:OnEnable(frame)
@@ -13,19 +13,15 @@ function Runes:OnEnable(frame)
 		for id=1, 6 do
 			local rune = CreateFrame("StatusBar", nil, frame.runeBar)
 			rune:SetFrameLevel(1)
-			rune.id = RUNE_MAP[id]
-			rune.id = id
 			
 			if( id > 1 ) then
-				rune:SetPoint("TOPLEFT", frame.runeBar.runes[id - 1], "TOPRIGHT", 1, 0)
+				rune:SetPoint("TOPLEFT", frame.runeBar.runes[RUNE_MAP[id - 1]], "TOPRIGHT", 1, 0)
 			else
 				rune:SetPoint("TOPLEFT", frame.runeBar, "TOPLEFT", 0, 0)
 			end
 			
-			table.insert(frame.runeBar.runes, rune)
+			frame.runeBar.runes[RUNE_MAP[id]] = rune
 		end
-
-		runeColors = {{r = 1, g = 0, b = 0.4}, {r = 0, g = 1, b = 0.4}, {r = 0, g = 0.4, b = 1}, {r = 0.7, g = 0.5, b = 1}}
 	end
 	
 	frame:RegisterNormalEvent("RUNE_POWER_UPDATE", self, "UpdateUsable")
@@ -43,7 +39,9 @@ function Runes:OnLayoutApplied(frame)
 		local barWidth = (frame.runeBar:GetWidth() - 5) / 6
 		
 		for id, rune in pairs(frame.runeBar.runes) do
+			local color = runeColors[GetRuneType(id)]
 			rune:SetStatusBarTexture(ShadowUF.Layout.mediaPath.statusbar)
+			rune:SetStatusBarColor(color.r, color.g, color.b)
 			rune:SetHeight(frame.runeBar:GetHeight())
 			rune:SetWidth(barWidth)
 		end
@@ -62,32 +60,29 @@ local function runeMonitor(self, elapsed)
 end
 
 -- Updates the timers on runes
-function Runes:UpdateUsable(frame, event, rune, available)
-	for _, indicator in pairs(frame.runeBar.runes) do
-		if( not rune or indicator.id == rune ) then
-			local startTime, cooldown, cooled = GetRuneCooldown(indicator.id)
-			if( not cooled ) then
-				indicator.endTime = GetTime() + cooldown
-				indicator:SetMinMaxValues(startTime, indicator.endTime)
-				indicator:SetValue(GetTime())
-				indicator:SetAlpha(0.50)
-				indicator:SetScript("OnUpdate", runeMonitor)
-			else
-				indicator:SetMinMaxValues(0, 1)
-				indicator:SetValue(1)
-				indicator:SetAlpha(1.0)
-				indicator:SetScript("OnUpdate", nil)
-			end
-		end
+function Runes:UpdateUsable(frame, event, id, usable)
+	if( not id or not frame.runeBar.runes[id] ) then return end
+	
+	local rune = frame.runeBar.runes[id]
+	local startTime, cooldown, cooled = GetRuneCooldown(id)
+	if( not cooled ) then
+		rune.endTime = GetTime() + cooldown
+		rune:SetMinMaxValues(startTime, rune.endTime)
+		rune:SetValue(GetTime())
+		rune:SetAlpha(0.40)
+		rune:SetScript("OnUpdate", runeMonitor)
+	else
+		rune:SetMinMaxValues(0, 1)
+		rune:SetValue(1)
+		rune:SetAlpha(1.0)
+		rune:SetScript("OnUpdate", nil)
 	end
 end
 
 -- No rune is passed for full update (Login), a single rune is passed when a single rune type changes, such as Blood Tap
-function Runes:Update(frame, event, rune)
-	for _, indicator in pairs(frame.runeBar.runes) do
-		if( not rune or indicator.id == rune ) then
-			local color = runeColors[GetRuneType(indicator.id)]
-			indicator:SetStatusBarColor(color.r, color.g, color.b)
-		end
+function Runes:Update(frame, event, id)
+	if( id ) then
+		local color = runeColors[GetRuneType(id)]
+		frame.runeBar.runes[id]:SetStatusBarColor(color.r, color.g, color.b)
 	end
 end
