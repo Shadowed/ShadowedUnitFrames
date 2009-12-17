05113ef40fb3a3f80a4140c4822ec85a3c937faa
Shadowed <shadowed.wow@gmail.com>
2009-12-16 16:16:11 -0800

    Switched to checking GetCVar("uiScale") to calculate effective scaling due to changes in when frames load

--- modules/layout.lua
+++ modules/layout.lua
@@ -272,7 +272,10 @@ function Layout:AnchorFrame(parent, frame, config)
 	local relativePoint = config.relativePoint and config.relativePoint ~= "" and config.relativePoint or preDefRelative[config.anchorPoint] or "CENTER"
 
 	-- Effective scaling is only used for unit based frames and if they are anchored to UIParent
-	local scale = config.anchorTo == "UIParent" and frame.unitType and GetCVarBool("useUiScale") and frame:GetEffectiveScale() or 1
+	local scale = 1
+	if( config.anchorTo == "UIParent" and frame.unitType and GetCVar("useUiScale") ) then
+		scale = frame:GetScale() * GetCVar("uiScale")
+	end
 	
 	frame:ClearAllPoints()
 	frame:SetPoint(point, anchorTo, relativePoint, config.x / scale, config.y / scale)
