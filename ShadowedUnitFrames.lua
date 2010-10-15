bf5128296504a607766e9c8410397e462e7a1af5
Hendrik Leppkes <h.leppkes@gmail.com>
2010-10-15 22:52:27 +0200

    Slightly adjust default positions for soulshard/holy power buttons.

--- ShadowedUnitFrames.lua
+++ ShadowedUnitFrames.lua
@@ -93,7 +93,7 @@ function ShadowUF:CheckUpgrade()
 			self.db.profile.units.player.soulShards.height = self.db.profile.units.player.soulShards.height or 0.40
 			self.db.profile.units.player.soulShards.enabled = true
 		end
-		self.db.profile.units.player.holyPower = {enabled = true, anchorTo = "$parent", order = 60, anchorPoint = "BR", x = -3, y = 8, size = 14, height = 0.40, spacing = -4, growth = "LEFT", isBar = true}
+		self.db.profile.units.player.holyPower = {enabled = true, anchorTo = "$parent", order = 60, anchorPoint = "BR", x = -3, y = 6, size = 14, height = 0.40, spacing = -4, growth = "LEFT", isBar = true}
 	end
 
 	-- July 1st
@@ -102,7 +102,7 @@ function ShadowUF:CheckUpgrade()
 		self.db.profile.units.player.fader.inactiveAlpha = self.db.profile.units.player.fader.inactiveAlpha or 0.6
 		self.db.profile.units.target.comboPoints.height = self.db.profile.units.target.comboPoints.height or 0.40
 		self.db.profile.units.player.eclipseBar = {enabled = true, background = true, height = 0.40, order = 70}
-		self.db.profile.units.player.soulShards = {enabled = true, anchorTo = "$parent", order = 60, anchorPoint = "BR", x = -3, y = 8, size = 14, height = 0.40, spacing = -4, growth = "LEFT", isBar = true}
+		self.db.profile.units.player.soulShards = {enabled = true, anchorTo = "$parent", order = 60, anchorPoint = "BR", x = -3, y = 6, size = 14, height = 0.40, spacing = -4, growth = "LEFT", isBar = true}
 		
 		self:LoadDefaultLayout(true)
 	end
