3c3a9a88ea96629e3e2d2017d9ba8163bbfcfd3b
Hendrik Leppkes <h.leppkes@gmail.com>
2010-10-17 12:37:21 +0200

    Updated IncHeal module to use the UNIT_HEAL_PREDICTION event instead of HealComm.

--- modules/incheal.lua
+++ modules/incheal.lua
@@ -1,14 +1,7 @@
-local HealComm = LibStub("LibHealComm-4.0", true)
-if( not HealComm ) then return end
-
 local IncHeal = {}
 local frames = {}
-local playerEndTime, playerGUID
 ShadowUF:RegisterModule(IncHeal, "incHeal", ShadowUF.L["Incoming heals"])
 ShadowUF.Tags.customEvents["HEALCOMM"] = IncHeal
-	
--- How far ahead to show heals at most
-local INCOMING_SECONDS = 3
 
 function IncHeal:OnEnable(frame)
 	frames[frame] = true
@@ -16,9 +9,8 @@ function IncHeal:OnEnable(frame)
 	
 	frame:RegisterUnitEvent("UNIT_MAXHEALTH", self, "UpdateFrame")
 	frame:RegisterUnitEvent("UNIT_HEALTH", self, "UpdateFrame")
+	frame:RegisterUnitEvent("UNIT_HEAL_PREDICTION", self, "UpdateFrame")
 	frame:RegisterUpdateFunc(self, "UpdateFrame")
-	
-	self:Setup()
 end
 
 function IncHeal:OnDisable(frame)
@@ -27,7 +19,6 @@ function IncHeal:OnDisable(frame)
 	
 	if( not frame.hasHCTag ) then
 		frames[frame] = nil
-		self:Setup()
 	end
 end
 
@@ -72,8 +63,6 @@ end
 function IncHeal:EnableTag(frame)
 	frames[frame] = true
 	frame.hasHCTag = true
-	
-	self:Setup()
 end
 
 function IncHeal:DisableTag(frame)
@@ -81,33 +70,7 @@ function IncHeal:DisableTag(frame)
 	
 	if( not frame.visibility.incHeal ) then
 		frames[frame] = nil
-		self:Setup()
 	end
-end
-
--- Check if we need to register callbacks
-function IncHeal:Setup()
-	playerGUID = UnitGUID("player")
-	
-	local enabled
-	for frame in pairs(frames) do
-		enabled = true
-		break
-	end
-	
-	if( not enabled ) then
-		if( HealComm ) then
-			HealComm:UnregisterAllCallbacks(IncHeal)
-		end
-		return
-	end
-
-	HealComm.RegisterCallback(self, "HealComm_HealStarted", "HealComm_HealUpdated")
-	HealComm.RegisterCallback(self, "HealComm_HealStopped")
-	HealComm.RegisterCallback(self, "HealComm_HealDelayed", "HealComm_HealUpdated")
-	HealComm.RegisterCallback(self, "HealComm_HealUpdated")
-	HealComm.RegisterCallback(self, "HealComm_ModifierChanged")
-	HealComm.RegisterCallback(self, "HealComm_GUIDDisappeared")
 end
 
 -- Update any tags using HC
@@ -124,9 +87,7 @@ end
 
 local function updateHealthBar(frame, interrupted)
 	-- This makes sure that when a heal like Tranquility is cast, it won't show the entire cast but cap it at 4 seconds into the future
-	local time = GetTime()
-	local timeBand = playerEndTime and math.min(playerEndTime - time, INCOMING_SECONDS) or INCOMING_SECONDS
-	local healed = (HealComm:GetHealAmount(frame.unitGUID, HealComm.ALL_HEALS, time + timeBand) or 0) * HealComm:GetHealModifier(frame.unitGUID)
+	local healed = UnitGetIncomingHeals(frame.unit) or 0
 	
 	-- Update any tags that are using HC data
 	IncHeal:UpdateTags(frame, healed)
@@ -163,33 +124,4 @@ end
 
 function IncHeal:UpdateFrame(frame)
 	updateHealthBar(frame, true)
-end
-
-function IncHeal:UpdateIncoming(interrupted, ...)
-	for frame in pairs(frames) do
-		for i=1, select("#", ...) do
-			if( select(i, ...) == frame.unitGUID ) then
-				updateHealthBar(frame, interrupted)
-			end
-		end
-	end
-end
-
--- Handle callbacks from HealComm
-function IncHeal:HealComm_HealUpdated(event, casterGUID, spellID, healType, endTime, ...)
-	if( casterGUID == playerGUID and bit.band(healType, HealComm.CASTED_HEALS) > 0 ) then playerEndTime = endTime end
-	self:UpdateIncoming(nil, ...)
-end
-
-function IncHeal:HealComm_HealStopped(event, casterGUID, spellID, healType, interrupted, ...)
-	if( casterGUID == playerGUID and bit.band(healType, HealComm.CASTED_HEALS) > 0 ) then playerEndTime = nil end
-	self:UpdateIncoming(interrupted, ...)
-end
-
-function IncHeal:HealComm_ModifierChanged(event, guid)
-	self:UpdateIncoming(nil, guid)
-end
-
-function IncHeal:HealComm_GUIDDisappeared(event, guid)
-	self:UpdateIncoming(true, guid)
 end
