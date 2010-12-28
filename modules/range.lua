6d03c3adaf61bf8411684544fd5165967173324f
Hendrik Leppkes <h.leppkes@gmail.com>
2010-12-28 13:28:51 +0100

    Greatly simplified the range module which should fix units showing in/out of range, when they are actually not.

--- modules/range.lua
+++ modules/range.lua
@@ -1,32 +1,35 @@
 local Range = {
 	friendly = {["PRIEST"] = GetSpellInfo(2061), ["DRUID"] = GetSpellInfo(5185), ["PALADIN"] = GetSpellInfo(635), ["SHAMAN"] = GetSpellInfo(331)},
 	hostile = {["PRIEST"] = GetSpellInfo(585), ["DRUID"] = GetSpellInfo(5176), ["PALADIN"] = GetSpellInfo(62124), ["HUNTER"] = GetSpellInfo(75), ["WARLOCK"] = GetSpellInfo(686), ["SHAMAN"] = GetSpellInfo(403), ["MAGE"] = GetSpellInfo(133), ["DEATHKNIGHT"] = GetSpellInfo(49576)},
-	resurrect = {["PALADIN"] = GetSpellInfo(7328), ["PRIEST"] = GetSpellInfo(2006), ["SHAMAN"] = GetSpellInfo(2008), ["DRUID"] = GetSpellInfo(50769)}
 }
 ShadowUF:RegisterModule(Range, "range", ShadowUF.L["Range indicator"])
 
 local playerClass = select(2, UnitClass("player"))
-local friendlySpell, hostileSpell
-local resurrectSpell = Range.resurrect[playerClass]
+local friendlySpell = Range.friendly[playerClass]
+local hostileSpell = Range.hostile[playerClass]
 
 local function checkRange(self, elapsed)
 	self.timeElapsed = self.timeElapsed + elapsed
 	if( self.timeElapsed <= 0.50 ) then return end
 	self.timeElapsed = 0
 	
-	--if( self.isFriendly and resurrectSpell and UnitIsDead(self.parent.unit) ) then
-	--	self.parent:SetRangeAlpha(IsSpellInRange(resurrectSpell, self.parent.unit) == 1 and ShadowUF.db.profile.units[self.parent.unitType].range.inAlpha or ShadowUF.db.profile.units[self.parent.unitType].range.oorAlpha)
-	-- We set a spell for them in our flags check, use that
-	if( self.spell ) then
-		self.parent:SetRangeAlpha(IsSpellInRange(self.spell, self.parent.unit) == 1 and ShadowUF.db.profile.units[self.parent.unitType].range.inAlpha or ShadowUF.db.profile.units[self.parent.unitType].range.oorAlpha)
+	local frame = self.parent
+	local spell
+	-- check which spell to use
+	if UnitIsFriend("player", frame.unit) then
+		spell = friendlySpell
+	elseif UnitCanAttack("player", frame.unit) then
+		spell = hostileSpell
+	end
+
+	if( spell ) then
+		self.parent:SetRangeAlpha(IsSpellInRange(spell, frame.unit) == 1 and ShadowUF.db.profile.units[frame.unitType].range.inAlpha or ShadowUF.db.profile.units[frame.unitType].range.oorAlpha)
 	-- That didn't work, but they are grouped lets try the actual API for this, it's a bit flaky though and not that useful generally
-	elseif( self.grouped ) then
-		self.parent:SetRangeAlpha(UnitInRange(self.parent.unit, "player") and ShadowUF.db.profile.units[self.parent.unitType].range.inAlpha or ShadowUF.db.profile.units[self.parent.unitType].range.oorAlpha)
+	elseif( UnitInRaid(frame.unit) or UnitInParty(frame.unit) ) then
+		self.parent:SetRangeAlpha(UnitInRange(frame.unit, "player") and ShadowUF.db.profile.units[frame.unitType].range.inAlpha or ShadowUF.db.profile.units[frame.unitType].range.oorAlpha)
 	-- Nope, fall back to interaction :(
-	elseif( self.isFriendly ) then
-		self.parent:SetRangeAlpha(CheckInteractDistance(self.parent.unit, 4) and ShadowUF.db.profile.units[self.parent.unitType].range.inAlpha or ShadowUF.db.profile.units[self.parent.unitType].range.oorAlpha)
 	else
-		self.parent:SetRangeAlpha(ShadowUF.db.profile.units[self.parent.unitType].range.inAlpha)
+		self.parent:SetRangeAlpha(CheckInteractDistance(frame.unit, 4) and ShadowUF.db.profile.units[frame.unitType].range.inAlpha or ShadowUF.db.profile.units[frame.unitType].range.oorAlpha)
 	end
 end
 
@@ -40,24 +43,14 @@ function Range:OnEnable(frame)
 		frame.range:SetScript("OnUpdate", checkRange)
 		frame.range.timeElapsed = 0
 		frame.range.parent = frame
-		frame.range:Hide()
+		frame.range:Show()
 	end
-	
-	-- I want to say UNIT_FACTION is the function thats called when a unit is MCed, but not 100% sure
-	frame:RegisterUnitEvent("UNIT_FACTION", self, "UpdateFlags")
-	frame:RegisterUnitEvent("UNIT_CONNECTION", self, "UpdateFlags")
-	frame:RegisterNormalEvent("PARTY_MEMBERS_CHANGED", self, "UpdateFlags")
-	frame:RegisterNormalEvent("RAID_ROSTER_UPDATE", self, "UpdateFlags")
-
-	frame:RegisterUpdateFunc(self, "UpdateFlags")
 	frame:RegisterUpdateFunc(self, "ForceUpdate")
 end
 
 function Range:OnLayoutApplied(frame)
-	if( frame.visibility.range ) then
-		frame.range.hostileSpell = ShadowUF.db.profile.range["hostile" .. playerClass] or self.hostile[playerClass]
-		frame.range.friendlySpell = ShadowUF.db.profile.range["friendly" .. playerClass] or self.friendly[playerClass]
-	end
+	hostileSpell = ShadowUF.db.profile.range["hostile" .. playerClass] or self.hostile[playerClass]
+	friendlySpell = ShadowUF.db.profile.range["friendly" .. playerClass] or self.friendly[playerClass]
 end
 
 function Range:OnDisable(frame)
@@ -68,21 +61,3 @@ function Range:OnDisable(frame)
 		frame:SetRangeAlpha(1.0)
 	end
 end
-
--- I'd rather store the flags here, they rarely change and we can do that based off events, no sense in doing it eveyr 0.50s
-function Range:UpdateFlags(frame)
-	frame.range.canAttack = UnitCanAttack("player", frame.unit)
-	frame.range.isFriendly = UnitIsFriend("player", frame.unit) and UnitCanAssist("player", frame.unit)
-	frame.range.grouped = UnitInRaid(frame.unit) or UnitInParty(frame.unit)
-	frame.range.spell = frame.range.canAttack and frame.range.hostileSpell or frame.range.isFriendly and frame.range.friendlySpell or nil
-	
-	-- No sense in updating range if we have no data
-	if( UnitIsGhost(frame.unit) or not UnitIsConnected(frame.unit) or ( not frame.range.spell and not frame.range.grouped and not frame.range.isFriendly ) ) then
-		frame:SetRangeAlpha(ShadowUF.db.profile.units[frame.unitType].range.inAlpha)
-		frame.range:Hide()
-	else
-		frame.range:Show()
-	end
-end
-
-
