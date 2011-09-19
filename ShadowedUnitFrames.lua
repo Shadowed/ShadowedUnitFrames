bb0a6df0d2e43331cfee41589b166602ae891330
Shadowed <shadowed.wow@gmail.com>
2011-09-18 23:33:32 -0700

    Syntax

--- ShadowedUnitFrames.lua
+++ ShadowedUnitFrames.lua
@@ -430,10 +430,10 @@ function ShadowUF:HideBlizzardFrames()
 		PlayerFrame.Show = self.noop
 		PlayerFrame:Hide()
 
-		PlayerFrame:RegisterEvent('UNIT_ENTERING_VEHICLE')
-		PlayerFrame:RegisterEvent('UNIT_ENTERED_VEHICLE')
-		PlayerFrame:RegisterEvent('UNIT_EXITING_VEHICLE')
-		PlayerFrame:RegisterEvent('UNIT_EXITED_VEHICLE')
+		PlayerFrame:RegisterEvent("UNIT_ENTERING_VEHICLE")
+		PlayerFrame:RegisterEvent("UNIT_ENTERED_VEHICLE")
+		PlayerFrame:RegisterEvent("UNIT_EXITING_VEHICLE")
+		PlayerFrame:RegisterEvent("UNIT_EXITED_VEHICLE")
 
 		PlayerFrameHealthBar:UnregisterAllEvents()
 		PlayerFrameManaBar:UnregisterAllEvents()
