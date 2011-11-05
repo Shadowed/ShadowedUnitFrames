766c859c89ac552b7c62c1e2199d92acc8b6439b
Shadowed <shadowed.wow@gmail.com>
2011-11-05 10:21:10 -0700

    Check both spellID and spell name in blacklist/whitelist for auras

--- modules/auras.lua
+++ modules/auras.lua
@@ -488,10 +488,10 @@ local function scan(parent, frame, type, config, filter)
 	local index = 0
 	while( true ) do
 		index = index + 1
-		local name, rank, texture, count, auraType, duration, endTime, caster, isRemovable = UnitAura(frame.parent.unit, index, filter)
+		local name, rank, texture, count, auraType, duration, endTime, caster, isRemovable, shouldConsolidate, spellID = UnitAura(frame.parent.unit, index, filter)
 		if( not name ) then break end
 		
-		if( ( not config.player or playerUnits[caster] ) and ( not parent.whitelist[type] and not parent.blacklist[type] or parent.whitelist[type] and parent.whitelist[name] or parent.blacklist[type] and not parent.blacklist[name] ) and ( not curable or canCure[auraType] ) ) then
+		if( ( not config.player or playerUnits[caster] ) and ( not parent.whitelist[type] and not parent.blacklist[type] or parent.whitelist[type] and ( parent.whitelist[name] or parent.whitelist[spellID] ) or parent.blacklist[type] and ( not parent.blacklist[name] and not parent.blacklist[spellID] ) ) and ( not curable or canCure[auraType] ) ) then
 			-- Create any buttons we need
 			frame.totalAuras = frame.totalAuras + 1
 			if( #(frame.buttons) < frame.totalAuras ) then
