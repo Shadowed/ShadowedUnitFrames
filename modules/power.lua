626132ef63cc3c3cfb05004d462205cbff1c8936
Shadowed <shadowed.wow@gmail.com>
2012-09-06 17:13:14 -0700

    Throttle power updating

--- modules/power.lua
+++ modules/power.lua
@@ -1,22 +1,9 @@
 local Power = {}
 ShadowUF:RegisterModule(Power, "powerBar", ShadowUF.L["Power bar"], true)
 
-local function updatePower(self, elapsed)
-	local currentPower = UnitPower(self.parent.unit)
-	if( currentPower == self.currentPower ) then return end
-	self.currentPower = currentPower
-
-	self:SetValue(currentPower)
-	for _, fontString in pairs(self.parent.fontStrings) do
-		if( fontString.fastPower ) then
-			fontString:UpdateTags()
-		end
-	end
-end
-
 function Power:OnEnable(frame)
 	frame.powerBar = frame.powerBar or ShadowUF.Units:CreateBar(frame)
-		
+	
 	frame:RegisterUnitEvent("UNIT_POWER_FREQUENT", self, "Update")
 	frame:RegisterUnitEvent("UNIT_MAXPOWER", self, "Update")
 	frame:RegisterUnitEvent("UNIT_CONNECTION", self, "Update")
@@ -28,22 +15,13 @@ function Power:OnEnable(frame)
 	frame:RegisterUpdateFunc(self, "Update")
 end
 
-function Power:OnLayoutApplied(frame)
-	-- Enable predicted updates which requires polling in an OnUpdate to get more up to date values
-	--if( frame.visibility.powerBar ) then
-	--	if( ShadowUF.db.profile.units[frame.unitType].powerBar.predicted ) then
-	--		frame.powerBar:SetScript("OnUpdate", updatePower)
-	--	else
-	--		frame.powerBar:SetScript("OnUpdate", nil)
-	--	end
-	--end
-end
-
 function Power:OnDisable(frame)
 	frame:UnregisterAll(self)
 end
 
 function Power:UpdateColor(frame)
+	frame.powerBar.currentType = select(2, UnitPowerType(frame.unit))
+
 	local color
 	if( ShadowUF.db.profile.units[frame.unitType].powerBar.colorType == "class" and UnitIsPlayer(frame.unit) ) then
 		local class = select(2, UnitClass(frame.unit))
@@ -51,7 +29,7 @@ function Power:UpdateColor(frame)
 	end
 	
 	if( not color ) then
-		color = ShadowUF.db.profile.powerColors[select(2, UnitPowerType(frame.unit))] or ShadowUF.db.profile.powerColors.MANA
+		color = ShadowUF.db.profile.powerColors[frame.powerBar.currentType] or ShadowUF.db.profile.powerColors.MANA
 	end
 	
 	if( not ShadowUF.db.profile.units[frame.unitType].powerBar.invert ) then
@@ -71,7 +49,9 @@ function Power:UpdateColor(frame)
 	end
 end
 
-function Power:Update(frame)
+function Power:Update(frame, event, unit, powerType)
+	if( event and powerType ~= frame.powerBar.currentType ) then return end
+
 	frame.powerBar.currentPower = UnitPower(frame.unit)
 	frame.powerBar:SetMinMaxValues(0, UnitPowerMax(frame.unit))
 	frame.powerBar:SetValue(UnitIsDeadOrGhost(frame.unit) and 0 or not UnitIsConnected(frame.unit) and 0 or frame.powerBar.currentPower)
