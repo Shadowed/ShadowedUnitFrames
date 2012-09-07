46080b5b6e95619e7f6d7c225f28c7fb7c4c23b4
Shadowed <shadowed.wow@gmail.com>
2012-09-06 17:40:08 -0700

    Redid the druid power bar, it will no longer flicker on/off when swapping bear <-> cat, but will still hide when you shift back into caster. Should be less annoying now

--- modules/druid.lua
+++ modules/druid.lua
@@ -4,7 +4,7 @@ ShadowUF:RegisterModule(Druid, "druidBar", ShadowUF.L["Druid mana bar"], true, "
 function Druid:OnEnable(frame)
 	frame.druidBar = frame.druidBar or ShadowUF.Units:CreateBar(frame)
 
-	frame:RegisterNormalEvent("UPDATE_SHAPESHIFT_FORM", self, "PowerChanged")
+	frame:RegisterUnitEvent("UNIT_DISPLAYPOWER", self, "PowerChanged")
 	
 	frame:RegisterUpdateFunc(self, "PowerChanged")
 	frame:RegisterUpdateFunc(self, "Update")
@@ -15,38 +15,36 @@ function Druid:OnDisable(frame)
 end
 
 function Druid:OnLayoutApplied(frame)
-	if( frame.visibility.druidBar ) then
-		local color = ShadowUF.db.profile.powerColors.MANA
-		
-		if( not ShadowUF.db.profile.units[frame.unitType].druidBar.invert ) then
-			frame.druidBar:SetStatusBarColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.alpha)
-			if( not frame.druidBar.background.overrideColor ) then
-				frame.druidBar.background:SetVertexColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.backgroundAlpha)
-			end
-		else
-			frame.druidBar.background:SetVertexColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.alpha)
+	if( not frame.visibility.druidBar ) then return end
 
-			color = frame.druidBar.background.overrideColor or color
-			frame.druidBar:SetStatusBarColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.backgroundAlpha)
+	local color = ShadowUF.db.profile.powerColors.MANA
+	
+	if( not ShadowUF.db.profile.units[frame.unitType].druidBar.invert ) then
+		frame.druidBar:SetStatusBarColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.alpha)
+		if( not frame.druidBar.background.overrideColor ) then
+			frame.druidBar.background:SetVertexColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.backgroundAlpha)
 		end
+	else
+		frame.druidBar.background:SetVertexColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.alpha)
+
+		color = frame.druidBar.background.overrideColor or color
+		frame.druidBar:SetStatusBarColor(color.r, color.g, color.b, ShadowUF.db.profile.bars.backgroundAlpha)
 	end
 end
 
 function Druid:PowerChanged(frame)
-	local form = GetShapeshiftFormID()
-	if( form == CAT_FORM or form == BEAR_FORM ) then
-		frame:RegisterUnitEvent("UNIT_POWER_FREQUENT", self, "Update")
-		frame:RegisterUnitEvent("UNIT_MAXPOWER", self, "Update")
-		ShadowUF.Layout:SetBarVisibility(frame, "druidBar", true)
-	else
-		frame:UnregisterSingleEvent("UNIT_POWER_FREQUENT", self)
-		frame:UnregisterSingleEvent("UNIT_MAXPOWER", self)
-		ShadowUF.Layout:SetBarVisibility(frame, "druidBar", nil)
-	end
+	local visible = UnitPowerType(frame.unit) ~= ADDITIONAL_POWER_BAR_INDEX
+	local type = visible and "RegisterUnitEvent" or "UnregisterSingleEvent"
+
+	frame[type](frame, "UNIT_POWER_FREQUENT", self, "Update")
+	frame[type](frame, "UNIT_POWERMAX", self, "Update")
+	ShadowUF.Layout:SetBarVisibility(frame, "druidBar", visible)
+
+	if( visible ) then self:Update(frame) end
 end
 
 function Druid:Update(frame, event, unit, powerType)
-	if( powerType ~= "MANA" ) then return end
+	if( powerType and powerType ~= "MANA" ) then return end
 	frame.druidBar:SetMinMaxValues(0, UnitPowerMax(frame.unit, SPELL_POWER_MANA))
 	frame.druidBar:SetValue(UnitIsDeadOrGhost(frame.unit) and 0 or not UnitIsConnected(frame.unit) and 0 or UnitPower(frame.unit, SPELL_POWER_MANA))
 end
