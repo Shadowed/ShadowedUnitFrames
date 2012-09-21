cb67af25da0ffa99d6517901fd6d32e26904e50c
Shadowed <shadowed.wow@gmail.com>
2012-09-20 23:17:13 -0700

    Druid mana bar will no longer show while in a vehicle

--- modules/druid.lua
+++ modules/druid.lua
@@ -33,7 +33,7 @@ function Druid:OnLayoutApplied(frame)
 end
 
 function Druid:PowerChanged(frame)
-	local visible = UnitPowerType(frame.unit) ~= ADDITIONAL_POWER_BAR_INDEX
+	local visible = UnitPowerType(frame.unit) ~= ADDITIONAL_POWER_BAR_INDEX and not frame.inVehicle
 	local type = visible and "RegisterUnitEvent" or "UnregisterSingleEvent"
 
 	frame[type](frame, "UNIT_POWER_FREQUENT", self, "Update")
