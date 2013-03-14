d1f21f97fe72efe6784cbe5f96e196804bb777e7
Shadowed <shadowed.wow@gmail.com>
2013-03-14 08:41:56 -0700

    Fixed Stagger bar being reset on reload

--- ShadowedUnitFrames.lua
+++ ShadowedUnitFrames.lua
@@ -5,7 +5,7 @@
 ShadowUF = select(2, ...)
 
 local L = ShadowUF.L
-ShadowUF.dbRevision = 34
+ShadowUF.dbRevision = 35
 ShadowUF.playerUnit = "player"
 ShadowUF.enabledUnits = {}
 ShadowUF.modules = {}
