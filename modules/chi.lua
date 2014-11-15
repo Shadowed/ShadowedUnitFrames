d3e80e0111205839beb99c9cc7f59dc7ae06fa20
Shadowed <shadowed.wow@gmail.com>
2014-11-15 10:38:51 -0800

    Cleaned up Chi code

--- modules/chi.lua
+++ modules/chi.lua
@@ -1,6 +1,6 @@
 local Chi = setmetatable({}, {__index = ShadowUF.ComboPoints})
 ShadowUF:RegisterModule(Chi, "chi", ShadowUF.L["Chi"], nil, "MONK")
-local chiConfig = {max = 5, key = "chi", colorKey = "CHI", powerType = SPELL_POWER_CHI, eventType = "CHI", icon = "Interface\\AddOns\\ShadowedUnitFrames\\media\\textures\\combo"}
+local chiConfig = {max = 6, key = "chi", colorKey = "CHI", powerType = SPELL_POWER_CHI, eventType = "CHI", icon = "Interface\\AddOns\\ShadowedUnitFrames\\media\\textures\\combo"}
 
 function Chi:OnEnable(frame)
 	frame.chi = frame.chi or CreateFrame("Frame", nil, frame)
@@ -22,20 +22,6 @@ function Chi:OnLayoutApplied(frame, config)
 	self:UpdateBarBlocks(frame)
 end
 
-function Chi:Update(frame, event, unit, powerType)
-	if( powerType and powerType ~= chiConfig.eventType ) then return end
-	
-	local points = UnitPower("player", chiConfig.powerType)
-	-- Bar display, hide it if we don't have any combo points
-	if( ShadowUF.db.profile.units[frame.unitType].chi.isBar ) then
-		ShadowUF.Layout:SetBarVisibility(frame, "chi", ShadowUF.db.profile.units[frame.unitType].chi.showAlways or (points and points > 0))
-	end
-	
-	for id, pointTexture in pairs(frame.chi.points) do
-		if( id <= points ) then
-			pointTexture:Show()
-		else
-			pointTexture:Hide()
-		end
-	end
+function Chi:GetPoints(unit)
+	return UnitPower("player", chiConfig.powerType)
 end
