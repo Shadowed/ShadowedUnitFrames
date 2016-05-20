fc00233b0eb9fd17f43e1ebae95dab04c0e909b9
Hendrik Leppkes <h.leppkes@gmail.com>
2016-05-20 12:30:36 +0200

    Update Monk Chi bar, 5 Chi and only for Windwalker

--- modules/chi.lua
+++ modules/chi.lua
@@ -1,8 +1,8 @@
 if( not ShadowUF.ComboPoints ) then return end
 
 local Chi = setmetatable({}, {__index = ShadowUF.ComboPoints})
-ShadowUF:RegisterModule(Chi, "chi", ShadowUF.L["Chi"], nil, "MONK")
-local chiConfig = {max = 6, key = "chi", colorKey = "CHI", powerType = SPELL_POWER_CHI, eventType = "CHI", icon = "Interface\\AddOns\\ShadowedUnitFrames\\media\\textures\\combo"}
+ShadowUF:RegisterModule(Chi, "chi", ShadowUF.L["Chi"], nil, "MONK", SPEC_MONK_WINDWALKER)
+local chiConfig = {max = 5, key = "chi", colorKey = "CHI", powerType = SPELL_POWER_CHI, eventType = "CHI", icon = "Interface\\AddOns\\ShadowedUnitFrames\\media\\textures\\combo"}
 
 function Chi:OnEnable(frame)
 	frame.chi = frame.chi or CreateFrame("Frame", nil, frame)
